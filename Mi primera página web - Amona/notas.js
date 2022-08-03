da121b48ac16a28fced0919d90841b88072d4106
franGiannico <86988983+franGiannico@users.noreply.github.com>
2022-08-03 01:31:48 -0300

    Add files via upload

---
+++ Mi primera página web - Amona/notas.js
@@ -0,0 +1,64 @@
+//              1. Preparación del Proyecto
+
+/*Requerimientos básicos: 
+. Inicio (home)
+    - Logo y menú (header)
+    - Banner
+        Título
+        Descripción
+        Botón
+        Imagen
+    - Servicios que ofrecemos
+        Desarrollo web
+        Desarrollo movil
+        Sistemas a medida
+    - Banner Corporativo
+        Imagen de fondo
+        Texto Principal
+        Texto Secundario
+        Botón que lleve a contacto
+    - Últimos Proyectos
+        4 Proyectos
+        Imagen (Screenshots)
+        Título (nombre del cliente)
+        Breve descripción
+. Pie de página (footer)
+        ¿Quieres trabajar con nosotros?
+            Texto adicional
+            Botón de contacto
+        Redes sociales
+        Copyright
+. ¿Quiénes somos?
+    - Historia
+        Imagen
+        Descripción
+    - Visión
+        Imagen
+        Descripción
+    - Misión 
+        Imagen
+        Descripción
+. Proyectos
+    Cada Proyecto
+        Imagen (Screenshot)
+        Nombre del cliente o proyecto
+        Descripción
+        Link
+. Contacto
+    - Datos de la empresa
+        Teléfono
+        Dirección
+        Correo
+        Mapa de Ubicación
+    - Formulario de Contacto
+        Integración con algún servicio*/
+
+
+/* Wireframe: es el diseño del sitio web o una parte del mismo, como si fuera un plano de una casa o una maqueta.
+
+Una vez que tengamos los Wireframes ya listos, debemos llevarselos al cliente para que él nos de el ok o
+haga todas las modificaciones que crea necesarias. */
+
+/*TRUCASO: para escribir en varios lugares o etiquetas a la vez, osea para no hacer copiar y pegar, 
+sino que directamente escribir código en varias etiquetas a la vez: alt + click en el espacio para escribir
+entre las etiquetas. */
